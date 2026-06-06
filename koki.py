ab16f72008aa4b03eac90fb0f36a02a73173fc02
krishkumarvl <kk5805705@gmail.com>
2026-06-06 21:41:40 +0530

    Day 2 mein kuch codes sikhe ai se aur samjhe

--- koki.py
+++ koki.py
@@ -1,9 +1,9 @@
 # Project KOKI — Day 2
 # Dictionary brain + session memory
-
+   #random isiliye use kra taki thoda robotic feel na aaye....
 import random
 
-responses = {
+responses = { #bahut se random responses daal diye ab ek word pr iske pass 3 alag alag response ho skte hai for exam hello type krne pr hi there bhi aa skta hai aur kya haal hai bhi...{aage dikkat nhi aani chahiye}
     "hello": ["Hey! Good to see you.", "Hello! Kya haal hai?", "Hi there!"],
     "how are you": ["Getting smarter every day!", "Learning and growing!"],
     "motivate me": ["Har mushkil ek naya lesson hai.", "Tu kar sakta hai bhai.", "Keep going. KOKI believes in you."],
@@ -16,7 +16,7 @@ responses = {
 }
 
 conversation_count = 0
-
+        # def function iska response decide krega based on use input
 def greet():
     name = input("KOKI: What's your name? ")
     print("KOKI: Hello " + name + "! I am KOKI. Still learning, but I'm here.")
@@ -28,6 +28,7 @@ def listen():
 def respond(msg, name):
     global conversation_count
     conversation_count += 1
+    #matching ke liye use kra lower case ka taki Hello ya Fir hello same lage
     msg = msg.lower().strip()
     
     for key in responses:
